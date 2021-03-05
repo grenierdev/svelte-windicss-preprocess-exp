3adc8040ada246a8ea39b4b383f1e91efdd3f0d5
Michael Grenier <michael@grenier.dev>
2021-03-05 12:07:11 -0500

    Added more syntax test

--- src/syntax.spec.ts
+++ src/syntax.spec.ts
@@ -2,16 +2,18 @@ import { Processor } from 'windicss/lib';
 import { expect } from 'chai';
 
 describe('Supported syntax', () => {
-	it('html5', () => {
+	it('vanilla', () => {
 		const processor = new Processor();
-		expect(processor.compile(`bg-gray-100 text-white`).success).to.be.eql(['bg-gray-100', 'text-white']);
+		expect(processor.compile(`min-h-screen bg-gray-100`).success).to.be.eql(['min-h-screen', 'bg-gray-100']);
+		expect(processor.compile(`bg-white font-light sm:hover:(bg-gray-100 font-medium)`).success).to.be.eql(['bg-white', 'font-light', 'sm:hover:bg-gray-100', 'sm:hover:font-medium']);
 	});
 	it('mustache', () => {
 		const processor = new Processor();
-		expect(processor.compile(`{isActive ? 'bg-amber-100' : 'bg-gray-100'}`).success).to.be.eql([]);
+		expect(processor.compile('flex px-{a} lg:px-{b}').success).to.be.eql(['flex']);
+		expect(processor.compile('bg-white font-light sm:hover:(bg-gray-{b} font-medium)').success).to.be.eql(['bg-white', 'font-light', 'sm:hover:font-medium']);
 	});
 	it('template literal', () => {
 		const processor = new Processor();
-		expect(processor.compile('`bg-amber-${shade}`').success).to.be.eql([]);
+		expect(processor.compile('px-3 py-${c + 2 - 10} text-gray-${d} text-sm font-medium').success).to.be.eql(['px-3', 'text-sm', 'font-medium']);
 	});
 })
